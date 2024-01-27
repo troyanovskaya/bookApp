6b8c8ca6c22eaf89ab05ba700f62f14d1a2c8dd4
Anastasiia <troyanovskaya2003@gmail.com>
2024-01-27 23:09:40 +0000

    header pannel created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookHeaderComponent } from './book-header/book-header.component';
+import { LogoComponent } from './book-header/logo/logo.component';
+import { NavigationComponent } from './book-header/navigation/navigation.component';
+import { SearchComponent } from './book-header/search/search.component';
+import { ProfileComponent } from './book-header/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookHeaderComponent,
+    LogoComponent,
+    NavigationComponent,
+    SearchComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/book-header/book-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-header',
+  templateUrl: './book-header.component.html',
+  styleUrls: ['./book-header.component.scss']
+})
+export class BookHeaderComponent {
+
+}
