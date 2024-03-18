67c0a78e547a2d345404e052472e21268363061e
Anastasiia <troyanovskaya2003@gmail.com>
2024-03-18 23:16:06 +0000

    @input refactor for book component, book-row styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,22 @@ import { VisibilityService } from './services/visibility.service';
 })
 export class AppComponent {
   title = 'bookApp';
+  books:{name:string, year: number, author:string, description:string[], img:string, state:string, saved:boolean}[]=
+  [
+    {name: "Jane Eyre", year:2011, author:"Charlotte Bronte", description:["Orphaned Jane Eyre endures an unhappy childhood, hated by her aunt and cousins and then sent to comfortless Lowood School. But life there improves and Jane stays on as a teacher, though she still longs for love and friendship. At Mr Rochester's house, where she goes to work as a governess, she hopes she might have found them - until she learns the terrible secret of the attic."],
+     img:"assets/covers/book1.jpg", state:"Unread", saved: false},
+     {name: "Jane Eyre", year:2011, author:"Charlotte Bronte", description:["Orphaned Jane Eyre endures an unhappy childhood, hated by her aunt and cousins and then sent to comfortless Lowood School. But life there improves and Jane stays on as a teacher, though she still longs for love and friendship. At Mr Rochester's house, where she goes to work as a governess, she hopes she might have found them - until she learns the terrible secret of the attic."],
+     img:"assets/covers/book1.jpg", state:"Unread", saved: false},
+     {name: "Jane Eyre", year:2011, author:"Charlotte Bronte", description:["Orphaned Jane Eyre endures an unhappy childhood, hated by her aunt and cousins and then sent to comfortless Lowood School. But life there improves and Jane stays on as a teacher, though she still longs for love and friendship. At Mr Rochester's house, where she goes to work as a governess, she hopes she might have found them - until she learns the terrible secret of the attic."],
+     img:"assets/covers/book1.jpg", state:"Unread", saved: false},
+     {name: "Jane Eyre", year:2011, author:"Charlotte Bronte", description:["Orphaned Jane Eyre endures an unhappy childhood, hated by her aunt and cousins and then sent to comfortless Lowood School. But life there improves and Jane stays on as a teacher, though she still longs for love and friendship. At Mr Rochester's house, where she goes to work as a governess, she hopes she might have found them - until she learns the terrible secret of the attic."],
+     img:"assets/covers/book1.jpg", state:"Unread", saved: false},
+     {name: "Jane Eyre", year:2011, author:"Charlotte Bronte", description:["Orphaned Jane Eyre endures an unhappy childhood, hated by her aunt and cousins and then sent to comfortless Lowood School. But life there improves and Jane stays on as a teacher, though she still longs for love and friendship. At Mr Rochester's house, where she goes to work as a governess, she hopes she might have found them - until she learns the terrible secret of the attic."],
+     img:"assets/covers/book1.jpg", state:"Unread", saved: false},
+     {name: "Jane Eyre", year:2011, author:"Charlotte Bronte", description:["Orphaned Jane Eyre endures an unhappy childhood, hated by her aunt and cousins and then sent to comfortless Lowood School. But life there improves and Jane stays on as a teacher, though she still longs for love and friendship. At Mr Rochester's house, where she goes to work as a governess, she hopes she might have found them - until she learns the terrible secret of the attic."],
+     img:"assets/covers/book1.jpg", state:"Unread", saved: false},
+     {name: "Jane Eyre", year:2011, author:"Charlotte Bronte", description:["Orphaned Jane Eyre endures an unhappy childhood, hated by her aunt and cousins and then sent to comfortless Lowood School. But life there improves and Jane stays on as a teacher, though she still longs for love and friendship. At Mr Rochester's house, where she goes to work as a governess, she hopes she might have found them - until she learns the terrible secret of the attic."],
+     img:"assets/covers/book1.jpg", state:"Unread", saved: false}
+  ]
   constructor(public visibilityService: VisibilityService){}
 }
--- src/app/components/book/book.component.ts
+++ src/app/components/book/book.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-book',
@@ -6,10 +6,8 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
   styleUrls: ['./book.component.scss']
 })
 export class BookComponent {
-  books:{name:string, year: number, author:string, description:string[], img:string, state:string, saved:boolean}[]=[
-    {name: "Jane Eyre", year:2011, author:"Charlotte Bronte", description:["Orphaned Jane Eyre endures an unhappy childhood, hated by her aunt and cousins and then sent to comfortless Lowood School. But life there improves and Jane stays on as a teacher, though she still longs for love and friendship. At Mr Rochester's house, where she goes to work as a governess, she hopes she might have found them - until she learns the terrible secret of the attic."],
-     img:"assets/covers/book1.jpg", state:"Unread", saved: false}
-  ]
+  @Input()
+    book?:{name:string, year: number, author:string, description:string[], img:string, state:string, saved:boolean}
   states:string[] = ["Unread", "Read", "Saved", "Dropped"]
   visible:boolean=false;
   currentState:string = this.states[0];
