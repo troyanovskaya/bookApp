52de57e0f433a215ca306bb2b5979c68c6facd39
Anastasiia <troyanovskaya2003@gmail.com>
2024-05-05 00:00:16 +0100

    comments display on the book page

--- src/app/components/book-page/book-page.component.ts
+++ src/app/components/book-page/book-page.component.ts
@@ -14,15 +14,15 @@ export class BookPageComponent implements OnInit {
   id: String = '';
   book?: Book;
   comm: String = '';
-  comments? = [{'_id': '1c', 'comment_book': '1b', 'comment_user': '1u', 'comment_user_avatar': 'assets/avatars/lotus.png', 'comment_user_login': 'user1', 'comment_text': 'drtyuvczxcvjc zxyhwedsrghcx st jhtg',
-  'comment_date': '16/11/2023'}]
   constructor( public booksService: BooksService,
      private route: ActivatedRoute, public logInService: LogInService,
      public userService: UserService){}
 
   ngOnInit(): void {
     this.route.params.subscribe( params => this.id = params['id']);
-    this.booksService.getBook(this.id).subscribe( book => this.book = book);
+    this.booksService.getBook(this.id).subscribe( book => {
+      this.book = book;
+    });
   }
   postComment(){
     let book = this.book;
@@ -31,18 +31,18 @@ export class BookPageComponent implements OnInit {
     let arrayOfRows = this.comm.split("\n");
     if(user && book){
       let book_comment = {user_id: user._id, user_login: user.user_login, user_img: user.user_img, comment: arrayOfRows, date: date.toString()};
-
       let user_comment = {book_id: book._id,  book_name: book.book_name, book_authors: book.book_authors, book_img: book.book_img, comment: arrayOfRows, date: date.toString()};
       console.log(user_comment)
       user.user_comments.push(user_comment);
+      localStorage.setItem('userObject', JSON.stringify(user))
       this.userService.patchUser(user, user._id).subscribe( data => {
         console.log(data);
       })
-      // book.book_comments.push(book_comment);
-      // this.book = book;
-      // this.booksService.patchBookArray(book_comment, 'book_comments', book._id).subscribe( data =>
-      //   console.log(data)
-      // );
+      book.book_comments.push(book_comment);
+      this.book = book;
+      this.booksService.patchBookArray(book_comment, 'book_comments', book._id).subscribe( data =>
+        console.log(data)
+      );
     }
 
   }
